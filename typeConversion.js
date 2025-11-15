5718566a6c601aa71664417d6d511507a575a025
simrantiwari-dev <157189422+simrantiwari17@users.noreply.github.com>
2025-11-16 01:09:55 +0530

    Add files via upload

---
+++ typeConversion.js
@@ -0,0 +1,3 @@
+console.log(Number("123"));
+console.log(String(true));
+console.log(Boolean(0));
---
+++ variablesinJS.js
@@ -0,0 +1,12 @@
+var name = "Simran";
+let age = 20;
+const country = "India";
+
+let arr = [1, 2, 3];
+let obj = {course: "Full Stack", day: 10};
+
+function greet() {
+  console.log("Welcome Simran!");
+}
+
+greet();
